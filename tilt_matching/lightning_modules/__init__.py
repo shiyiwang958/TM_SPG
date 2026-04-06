33ce69b3473d9acf2e5ea87b990dc52db73b32e1
yuyuanchen0 <yuyuan.chen0@gmail.com>
2026-04-05 22:09:39 -0400

    gsm only lightning import

--- tilt_matching/lightning_modules/__init__.py
+++ tilt_matching/lightning_modules/__init__.py
@@ -1,9 +1,3 @@
-from .tilt_sudoku import TiltSudokuModule
-from .tilt_countdown import TiltCountdownModule
 from .tilt_matching import TiltLogModule
 
-__all__ = [
-    "TiltSudokuModule",
-    "TiltCountdownModule",
-    "TiltLogModule",
-]
+__all__ = ["TiltLogModule"]
